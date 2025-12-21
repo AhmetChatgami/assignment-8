55f5238bfdcafd75ee5b202c016544ab93ee7deb
AhmedYeasin <ahmetchatgami@gmail.com>
2025-12-21 21:51:08 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Home from "../Pages/Home";
+import Apps from "../Pages/Apps";
+import Installation from "../Pages/Installation";
+import MainLayout from "../Layouts/MainLayout";
+import ErroPage from "../Pages/ErroPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErroPage />, 
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+
+      {
+        path: "/apps",
+        element: <Apps />,
+      },
+
+      {
+        path: "/installation",
+        element: <Installation />,
+      },
+
+    ],
+  },
+]);
+
+export default router;
